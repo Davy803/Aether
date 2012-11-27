76b88514d5735aa9206825bb1c814c43010f874d
David Ly <davy803@gmail.com>
2012-11-26 20:28:46 -0600

    Added CreatureAction

---
+++ ProjectAether/ProjectAether/app/Actions/CreatureAction.js
@@ -0,0 +1,59 @@
+var ProjectAether;
+(function (ProjectAether) {
+    (function (Actions) {
+        var CreatureAction = (function () {
+            function CreatureAction(board, creature) {
+                this.board = board;
+                this.creature = creature;
+                this.targetTypes = [
+                    ProjectAether.TargetTypes.Space, 
+                    ProjectAether.TargetTypes.Button, 
+                    ProjectAether.TargetTypes.Creature
+                ];
+            }
+            CreatureAction.prototype.targetValidator = function (target) {
+                if(target instanceof ProjectAether.Space) {
+                    var space = target;
+                    return this.board.findEmptySquaresWithinXSteps(this.creature.location(), this.creature.movement.current()).contains(space);
+                }
+                if(target instanceof ProjectAether.Creature) {
+                    return this._canAttack(target);
+                }
+                if(target instanceof ProjectAether.Button) {
+                    return target instanceof ProjectAether.CancelButton;
+                }
+                throw new ProjectAether.NotImplementedError();
+            };
+            CreatureAction.prototype.perform = function (target) {
+                if(target instanceof ProjectAether.CancelButton) {
+                    return null;
+                }
+                if(target instanceof ProjectAether.Space) {
+                    var newSpace = target;
+                    this.creature.move(newSpace, this.board.getDistance(this.creature, newSpace));
+                    this.creature.isSelected(false);
+                    return null;
+                } else {
+                    if(target instanceof ProjectAether.Creature) {
+                        var targetCreature = target;
+                        this.creature.attack(targetCreature);
+                    } else {
+                        throw Error("Invalid target");
+                    }
+                }
+            };
+            CreatureAction.prototype.performMulti = function (spaces) {
+                throw Error("Cannot target multiple spaces");
+            };
+            CreatureAction.prototype._canAttack = function (targetCreature) {
+                if(targetCreature) {
+                    return this.creature.canAttack() && this.board.spacesAreAdjacent(this.creature.location(), targetCreature.location());
+                }
+                return this.creature.canAttack();
+            };
+            return CreatureAction;
+        })();
+        Actions.CreatureAction = CreatureAction;        
+    })(ProjectAether.Actions || (ProjectAether.Actions = {}));
+    var Actions = ProjectAether.Actions;
+})(ProjectAether || (ProjectAether = {}));
---
+++ ProjectAether/ProjectAether/app/Actions/CreatureAction.ts
@@ -0,0 +1,64 @@
+/// <reference path="../main.ts" />
+
+module ProjectAether.Actions {
+    export class CreatureAction implements Action {
+        targetTypes = [TargetTypes.Space, TargetTypes.Button, TargetTypes.Creature];
+        constructor (private board: Board, private creature: Creature) {
+
+        }
+        targetValidator(target: Target) {
+            //Moving
+            if (target instanceof Space) {
+                var space = <Space> target;
+                return this.board.findEmptySquaresWithinXSteps(this.creature.location(), this.creature.movement.current()).contains(space);
+            }
+            //Attacking
+            if (target instanceof Creature) {
+                return this._canAttack(<Creature>target);
+            }
+
+            if (target instanceof Button) {
+                return target instanceof CancelButton;
+            }
+            throw new ProjectAether.NotImplementedError();
+        }
+        perform(target: Target): Action {
+            if (target instanceof CancelButton) {
+                return null;
+            }
+            //Move
+            if (target instanceof Space) {
+                var newSpace = <Space> target;
+                this.creature.move(newSpace, this.board.getDistance(this.creature, newSpace));
+                this.creature.isSelected(false);
+                return null;
+            } 
+            //Attack
+            else if (target instanceof Creature) {
+                var targetCreature = <Creature> target;
+                this.creature.attack(targetCreature);
+            }
+            else {
+                throw Error("Invalid target");
+            }
+        }
+
+        performMulti(spaces: Space[]) {
+            throw Error("Cannot target multiple spaces");
+        }
+
+        private _canAttack(targetCreature?: Creature){
+            if(targetCreature){
+                return this.creature.canAttack() && this.board.spacesAreAdjacent(this.creature.location(), targetCreature.location());
+            }
+            return this.creature.canAttack()
+        }
+        //private _getDistance(space1: Space, space2: Space){
+        //    var dx = Math.abs(space1.x - space2.x);
+        //    var dy = Math.abs(space1.y - space2.y);
+        //    var dz = Math.abs(space1.z() - space2.z());
+        //    return Math.max(dx, dy, dz);
+        //}
+
+    }
+}
